c1abc03805745d50672cf3b1902468489178631c
Mirza Fahad <83924757+fahadmirfa@users.noreply.github.com>
2022-07-17 15:03:18 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Ticket Booking App',
       theme: ThemeData(
         primaryColor: Primary,
       ),
